ae1dee58d5d0cf0517d99be8a716ff8b909d7e9d
Yassine Elmiri <144792335+yassineelmiri@users.noreply.github.com>
2024-06-30 01:54:05 +0100

    applique travaille sur g c login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from '../component/login/login.component';
 import { RegisterComponent } from '../component/register/register.component';
 import { DashboardComponent } from '../component/dashboard/dashboard.component';
+import { ContactusComponent } from '../component/contactus/contactus.component';
+import { NotfoundComponent } from '../component/notfound/notfound.component';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent, title: 'sing-in' },
     { path: 'regiter', component: RegisterComponent, title: 'sing-up' },
     {
