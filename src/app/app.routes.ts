09e60c8bf184f104b4e9761d1a3534402e47f05c
Yassine Elmiri <144792335+yassineelmiri@users.noreply.github.com>
2024-06-30 01:28:55 +0100

    ajouter routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../component/login/login.component';
+import { RegisterComponent } from '../component/register/register.component';
+import { DashboardComponent } from '../component/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent, title: 'sing-in' },
+    { path: 'regiter', component: RegisterComponent, title: 'sing-up' },
+    {
+      path: 'profil/:id',
+      component: DashboardComponent,
+      title: 'profil',
+    }
+  ];
+  
